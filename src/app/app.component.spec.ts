b37ab0b5c57b5ea28de68e6c5136e5a55fd675ec
ahmed mohamed <ahmedmohamed26.github.io>
2021-01-04 13:16:01 +0200

    unit test in home service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'trufla-task'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('trufla-task');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('trufla-task app is running!');
-  });
 });
--- src/app/pages/home/home.service.spec.ts
+++ src/app/pages/home/home.service.spec.ts
@@ -1,16 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
+import { IPerson, IPersons } from 'src/app/shared/models/people';
 import { HomeService } from './home.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
 
 describe('HomeService', () => {
   let service: HomeService;
-
+  let httpMock: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [HomeService],
+    });
     service = TestBed.inject(HomeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('be able to retrieve GET method', () => {
+    service.getPeople().subscribe((people: IPersons) => {
+      expect(people.results.length).toBe(10);
+      let req = httpMock.expectOne(`${environment.apiKey}/people/`);
+      expect(req.request.method).toBe('GET');
+      req.flush(people.results);
+      httpMock.verify();
+    });
   });
 });
